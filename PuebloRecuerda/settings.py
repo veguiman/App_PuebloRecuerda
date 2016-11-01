64a16c14f2e0690ca1e79f5327f6e829c7eaf0c5
JOEL SUAREZ <joekgs25@gmail.com>
2016-11-01 00:34:30 -0500

    Configuracion de aplicaciones

--- PuebloRecuerda/settings.py
+++ PuebloRecuerda/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'diputados.apps.DiputadosConfig',
+    'leyes.apps.LeyesConfig',
+    'home.apps.HomeConfig',
+    'seguridad.apps.SeguridadConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
